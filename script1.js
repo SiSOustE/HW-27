9b8f387723d0ba3982910701bdb9f61cc8e6b1dd
SiSOustE <ryzhevsky@gmail.com>
2024-11-01 00:42:35 +0300

    правка / ментор / Arrow function выводит undefined

--- script1.js
+++ script1.js
@@ -1,38 +1,22 @@
 /* I.
 функция, которая принимает массив символов, а возвращает слова
+*/
 
-//Вариант 1
-let semantics // осмысленный набор символов - слова
-function getWords (setOfSimbols) {
-semantics = setOfSimbols.join('');
-  return semantics;
+function getWords(setOfSymbols) {
+  return setOfSymbols.join('');
 }
-getWords(['с', 'а', 'м', 'ы', 'х', ' ', 'ч', 'е', 'с', 'т', 'н', 'ы', 'х', ' ', 'п', 'р', 'а', 'в', 'и', 'л']);
-console.log(semantics);*/
+
+const words = getWords(['с', 'а', 'м', 'ы', 'х', ' ', 'ч', 'е', 'с', 'т', 'н', 'ы', 'х', ' ', 'п', 'р', 'а', 'в', 'и', 'л']);
+console.log(words);
 
 // Вариант 2
-/*let semantics;
-function getWords (setOfSimbols) {
-  semantics = setOfSimbols.join('');
-  return semantics
-}*/
-
-/*const word1 = ['с', 'а', 'м', 'ы', 'х'];
-const word2 = ['ч', 'е', 'с', 'т', 'н', 'ы', 'х'];
-const word3 = ['п', 'р', 'а', 'в', 'и', 'л'];*/
-// const quote =( word1.concat([' ']).concat(word2).concat([' ']).concat(word3)).join('');
-// console.log(quote);
-
-// Вариант 3
-
-let quote
 function getQuote(word1, word2, word3) {
-  quote = (['"'].concat(word1).concat([' ']).concat(word2).concat([' ']).concat(word3).concat([`\"`])).join('');
-  return quote
+  return (['"'].concat(word1).concat([' ']).concat(word2).concat([' ']).concat(word3).concat(['"'])).join('');
 }
 
-getQuote(['с', 'а', 'м', 'ы', 'х'], ['ч', 'е', 'с', 'т', 'н', 'ы', 'х'], ['п', 'р', 'а', 'в', 'и', 'л']);
+const quote = getQuote(['с', 'а', 'м', 'ы', 'х'], ['ч', 'е', 'с', 'т', 'н', 'ы', 'х'], ['п', 'р', 'а', 'в', 'и', 'л']);
 console.log(quote);
+
 
 /* ------------------------------------------------------
 II.
@@ -43,19 +27,15 @@ const user = {
   age: 60
 };
 
-let userStrData;
-
-// Вариант 1
+// Function declare
 function convertObjToStr(user) {
-  userStrData = user.toString()
-  // userStrData = JSON.stringify(user);
-  return userStrData
+  return `Привет, ${user.name}, Вам ${user.age} лет!`
 }
-
-convertObjToStr(any1);
+const userStrData = convertObjToStr(user);
 console.log(userStrData);
 
-// Вариант 2
-convertObjToStr = (user) => console.log(`Привет, ${user.name}, Вам ${user.age} лет`);
-convertObjToStr = (user) => `Привет, ${user.name}, Вам ${user.age}`;
-concole.log(convertObjToStr)
+// Arrow function
+const converObjToStrArrow = (user) => {
+  `Привет, ${user.name}, Вам ${user.age} лет!`
+};
+console.log(converObjToStrArrow(user));
